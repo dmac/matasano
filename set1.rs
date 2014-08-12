c0ebe27baa19b4b7f65e38119ba83811c4368e7b
Daniel MacDougall <dmacdougall@gmail.com>
2014-08-12 15:34:20 -0700

    Challenge 5

--- set1.rs
+++ set1.rs
@@ -1,7 +1,7 @@
 // Alternate implementation of Set 1 using libraries for serialization.
 extern crate serialize;
 
-use serialize::hex::{FromHex};
+use serialize::hex::{FromHex, ToHex};
 use serialize::base64::{Config, Standard, ToBase64};
 
 use std::io::{BufferedReader, File};
@@ -17,13 +17,13 @@ fn hex_to_base64(s: &str) -> String {
 
 #[test]
 fn test_hex_to_base64() {
-    assert_eq!("TWFu".to_string(),
-               hex_to_base64("4d616e"));
+    assert_eq!("TWFu",
+               hex_to_base64("4d616e").as_slice());
     assert_eq!(
-        "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t".to_string(),
+        "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t",
         hex_to_base64("\
 49276d206b696c6c696e6720796f757220627261696e206c\
-696b65206120706f69736f6e6f7573206d757368726f6f6d"));
+696b65206120706f69736f6e6f7573206d757368726f6f6d").as_slice());
 }
 
 fn fixed_xor(b: &[u8], key: &[u8]) -> Vec<u8> {
@@ -37,9 +37,10 @@ fn fixed_xor(b: &[u8], key: &[u8]) -> Vec<u8> {
 
 #[test]
 fn test_fixed_xor() {
-    assert_eq!("746865206b696420646f6e277420706c6179".from_hex().unwrap(),
+    assert_eq!("746865206b696420646f6e277420706c6179",
                fixed_xor("1c0111001f010100061a024b53535009181c".from_hex().unwrap().as_slice(),
-                         "686974207468652062756c6c277320657965".from_hex().unwrap().as_slice()));
+                         "686974207468652062756c6c277320657965".from_hex().unwrap().as_slice())
+               .as_slice().to_hex().as_slice());
 }
 
 fn score_text_naive(s: &str) -> uint {
@@ -91,6 +92,25 @@ fn detect_single_character_xor(filename: &str) -> (String, char, int) {
         }
     }
     (best_plaintext, best_key, best_score)
+}
+
+fn repeating_key_xor(b: &[u8], key: &[u8]) -> Vec<u8> {
+    let mut key_buf = Vec::new();
+    let mut ki = 0;
+    for _ in range(0, b.len()) {
+        key_buf.push(key[ki]);
+        ki = (ki + 1) % key.len();
+    }
+    fixed_xor(b, key_buf.as_slice())
+}
+
+#[test]
+fn test_repeating_key_xor() {
+    assert_eq!("\
+0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272\
+a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f",
+               repeating_key_xor("Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal".as_bytes(),
+                                 "ICE".as_bytes()).as_slice().to_hex().as_slice());
 }
 
 fn main() {
